419986b0e0e0cf53a011439771d42663828b2e5c
AndyIfapme <andy.bianco@formateur.ifapme.be>
2023-05-08 21:15:29 +0200

    nullable

---
+++ Theory_Nullable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Theory_Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 0;
+            int? b = 11;
+
+            if (b.HasValue && b.Value > 10)
+            {
+                Console.WriteLine($"Votre valeur est {b.Value}");
+
+            }
+        }
+    }
+}
